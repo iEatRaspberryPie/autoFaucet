e77cb9b0593bca1e5dfcc52b73701155b37a41cb
iEatRaspberryPie <imbelligerent9834@gmail.com>
2024-11-30 06:52:58 -0600

    Update autoFaucet.ino

--- autoFaucet.ino
+++ autoFaucet.ino
@@ -1,3 +1,6 @@
+//This is the first test to see if the pump works. It turns the pump on when a button is pressed.
+//I have to replace the digital pins with analog ones to actually control pump power.
+
 const int button = 2;
 const int pump = 9;
 int buttonState;
